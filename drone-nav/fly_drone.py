5f71484d99fd1c863222bc9ae59f2460317c4d93
Jeremiah Whitehurst <jalexander-wv@protonmail.com>
2023-11-29 15:12:54 -0500

    Added recitation-7 program fly_drone

---
+++ drone-nav/fly_drone.py
@@ -0,0 +1,78 @@
+"""
+This module contains a function that continuously calls a function
+is_object_detected, which returns a True if a sensor detects an object
+otherwise it returns False.  If is_object_detected returns a True, then
+the avoid_object function is invoked to take action.
+
+
+TEST TO ADJUST FORWARD_POWER TO YOUR LIKING
+"""
+
+from codrone_edu.drone import *
+# import time
+
+DISTANCE_THRESHOLD = 60  # distance in cm in which an object is acknowledged to exist
+FORWARD_POWER = 15
+# within_a_distance=60
+
+drone = Drone()
+drone.pair()
+
+
+def is_object_detected(within_a_distance):
+    """
+    Returns a True if an object is less than or equal to
+    the value passed to within_a_distance; otherwise False.
+    """
+    return drone.get_front_range() <= within_a_distance
+
+
+def avoid_object():
+    """
+    Turns to the left and reads the front range sensor.
+    Turns to the right and reads the front range sensor.
+    If both left and right are blocked, then the drone goes back from where it came
+    otherwise it goes right if the right side is not block.  If the right side is
+    blocked, then the drone goes left if the left side is not blocked.
+     # drone.get_front_range()
+    # if drone.get_front_range() > within_a_distance:
+    #     drone.turn_degree(90)
+    #     if drone.get_front_range() > within_a_distance:
+    #         drone.turn_degree(180)
+    """
+    # code does not move if distance > 150
+    drone.set_pitch(0)
+    drone.turn_left(90)
+    left_turn = drone.get_front_range()
+    drone.hover(1)
+    drone.turn_right(180)
+    right_turn = drone.get_front_range()
+    drone.hover(1)
+    if right_turn > 150:
+        pass
+    elif left_turn > 150:
+        drone.turn_left(180)
+    else:
+        drone.turn_right(90)
+    drone.hover(1)
+    drone.set_pitch(FORWARD_POWER)
+    drone.move(3)
+
+
+def fly_drone():
+    drone.takeoff()
+    drone.set_pitch(FORWARD_POWER)
+    drone.move()
+    try:
+        while True:
+            drone.move()  # moves forward
+            if is_object_detected(DISTANCE_THRESHOLD):  # if an object is detected within 60 cms
+                avoid_object()  # execute avoid_object() function
+    except:
+        drone.set_pitch(0)  # set pitch to zero so the drone does not move
+    finally:
+        drone.land()  # when the try statement has concluded, land the drone
+        drone.close()  # close the connection
+
+
+fly_drone()
--- drone-nav/navigation.py
+++
@@ -1,1 +0,0 @@
-# Paste navigation program here
